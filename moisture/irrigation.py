687a92cdc1c694f9678766405f22e4bec4d3ee46
cseas <contact.abhijeetsingh@gmail.com>
2019-02-08 19:00:03 +0530

    Add new moisture code

--- moisture/irrigation.py
+++ moisture/irrigation.py
@@ -1,11 +1,25 @@
 from sklearn.cross_decomposition import PLSRegression
+# from sklearn.ensemble import RandomForestRegressor
 
 import pandas as pd
-X = pd.read_csv("dataset/input.csv")
-y = pd.read_csv("dataset/output.csv")
+X = pd.read_csv("weather_data.csv")
+y = pd.read_csv("water_required.csv")
+
+X = X.drop('date', axis = 1)
+
+# print(X.isnull().any())
+# fill empty values in the dataset
+X = X.fillna(method='ffill')
+# print("..........................")
+# print(X.isnull().any())
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+
+X_train = X_train.values
+X_test = X_test.values
+y_train = y_train.values
+y_test = y_test.values
 
 plsr = PLSRegression(
     n_components=2, 
@@ -14,17 +28,40 @@ plsr = PLSRegression(
     tol=1e-06, 
     copy=True)
 
-model = plsr.fit(X_train, y_train)
+# plsr = RandomForestRegressor(n_estimators = 1000, random_state = 42)
 
-def analyze(X_input):
-    global model
-    inputs = pd.read_csv(X_input)
-    pred = model.predict(inputs)
-    return pred
+# plsr
+# model = plsr.fit(X_train, y_train)
+# forest
+model = plsr.fit(X_train, y_train.ravel())
 
-if __name__ == "__main__":
-    # predict
-    predict = analyze(X_test)
-    # accuracy
-    from sklearn.metrics import accuracy_score
-    print(accuracy_score(y_test, predict))
+pred = model.predict(X_test)
+
+# accuracy
+# print(model.score(X_test, y_test))
+
+# for i in range(136):
+#     print(model.predict([X_test[i]]))
+
+# this is water required by crop in mm
+water_requirement = model.predict([X_test[0]])
+# convert to cm
+water_requirement = water_requirement/10
+
+def run_motor(soil_moisture):
+    global water_requirement
+    water_requirement = ((100 - soil_moisture) * 0.01) * (water_requirement)
+
+    # area of field
+    area = 12.5 * 8.5
+    # speed of motor in cm cube per sec
+    speed = 6.25
+    # water required scaled down by factor of 10 for demo
+    water_requirement = water_requirement/10
+
+    seconds = (water_requirement * area)/speed
+
+    # convert to int
+    seconds = int(seconds[0])
+
+    return seconds
